9c4c179d765bbdb233c76e5a4ac12960283e7ae0
Ryan Ardray <rardray@gmail.com>
2019-01-21 13:57:50 -0600

    finishing layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
   get '/contact', to: 'static_pages#contact'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
