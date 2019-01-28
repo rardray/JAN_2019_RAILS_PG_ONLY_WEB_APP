3342fa4d5e37b86bfc5581532f7ce30f78a0767a
Ryan Ardray <rardray@gmail.com>
2019-01-28 17:14:37 -0600

    signup tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
       if @user.save
-        ##
+        render 'show'
       else
         render 'new'
       end
