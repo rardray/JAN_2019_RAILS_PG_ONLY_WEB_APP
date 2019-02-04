63970bf2dee329be7ce385e6480d72081c574a93
Ryan Ardray <rardray@gmail.com>
2019-02-04 16:38:48 -0600

    session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/contact', to: 'static_pages#contact'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   root 'static_pages#home'
   resources :users
 end
