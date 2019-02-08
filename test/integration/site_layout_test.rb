93f6b1455960ac1f1918ed1f8d736340ec60f887
Ryan Ardray <rardray@gmail.com>
2019-02-08 01:26:17 -0600

    users index

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test 'layout links' do
+  def setup
+    @user = users(:test)
+  end
+  test 'layout links logged in' do
+    log_in_as(@user)
     get root_path
     assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path, count: 2
@@ -9,9 +13,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', contact_path
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', signup_path
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', edit_user_path(@user)
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a.dropdown-toggle', @user.name
     get contact_path
     assert_select 'title', full_title('Contact')
     get signup_path
     assert_select 'title', full_title('Sign Up')
+  end
+  test 'layout links logged out' do
+    get root_path
+    assert_select 'a[href=?]', edit_user_path(@user), count: 0
+    assert_select 'a.dropdown-toggle', @user.name.nil?
   end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,6 +5,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user = users(:test)
   end
   test 'unsuccessful edit' do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: '',
@@ -16,9 +17,11 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert'
   end
 
-  test 'successful edit' do
+  test 'successful edit with friendly fowarding' do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    assert_not !session[:forwarding_url]
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
     name = 'Foo Bar'
     email = 'email@email.com'
     patch user_path(@user), params: { user: { name: name,
@@ -29,7 +32,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
+    assert_nil session[:forwarding_url]
     assert_equal name, @user.name
     assert_equal email, @user.email
   end
+  
 end
